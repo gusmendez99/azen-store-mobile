d9d4bf3c26dbb9693093278b9d3f18a47e9e23b1
Gus Mendez <gus.mendez.99@gmail.com>
2020-06-04 11:07:59 -0600

    change password ui updated

--- src/screens/ChangePassword/index.js
+++ src/screens/ChangePassword/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
 //import Button from 'react-native-button';
 import { connect } from 'react-redux';
+import {
+  StyleSheet,
+  ImageBackground,
+  Dimensions,
+  KeyboardAvoidingView,
+  View
+} from 'react-native';
+import { Text, Input, Button, Block, Icon, theme } from '../../components/UIComponents';
 import { Field, reduxForm } from 'redux-form';
-import { Text, Input, Button } from '../../components/UIComponents'
+import { AppStyles } from '../../AppStyles';
 import AnimatedLoader from 'react-native-animated-loader';
 
 import * as actions from '../../redux/user/user.actions';
 import * as selectors from '../../redux/root-reducer';
 
-import { AppStyles } from '../../AppStyles';
+
+const { width, height } = Dimensions.get("screen");
 
 const renderInput = ({ input: { onChange, ...input }, ...rest }) => {
   return (
@@ -24,7 +32,7 @@ const renderInput = ({ input: { onChange, ...input }, ...rest }) => {
 const ChangePassword = ({ isChangingPassword, error, onChangePassword, handleSubmit }) => {
 
   return (
-    <View style={styles.container}>
+    /*<View style={styles.container}>
       <Text style={[styles.title, styles.centerTitle]}>Change your password</Text>
         <Field
           name={'oldPassword'}
@@ -85,37 +93,154 @@ const ChangePassword = ({ isChangingPassword, error, onChangePassword, handleSub
             }
 
       
-    </View>
+    </View>*/
+
+    <Block flex middle>
+      <ImageBackground
+        source={{ uri: "https://raw.githubusercontent.com/creativetimofficial/argon-react-native/master/assets/imgs/register-bg.png" }}
+        style={{ width, height, zIndex: 1, marginTop: 150 }}
+      >
+        <Block flex middle>
+          <Block style={styles.registerContainer}>
+            <Block flex={0.25} middle style={styles.socialConnect}>
+              <Text color="#8898AA" h5>
+                Change Password
+                </Text>
+            </Block>
+            <Block flex>
+              <Block flex center>
+                <KeyboardAvoidingView
+                  style={{ flex: 1 }}
+                  behavior="padding"
+                  enabled
+                >
+                  <Block width={width * 0.8} style={{ marginBottom: 15 }}>
+                    <Field
+                      name={'oldPassword'}
+                      props={{
+                        placeholder: 'Enter old password...',
+                        rounded: true,
+                        borderless: true,
+                        underlineColorAndroid: 'transparent',
+                        password: true,
+                        viewPass: true,
+                      }}
+                      component={renderInput}
+                    />
+
+                  </Block>
+                  <Block width={width * 0.8} style={{ marginBottom: 15 }}>
+                    <Field
+                      name={'newPassword1'}
+                      props={{
+                        placeholder: 'Enter new password...',
+                        rounded: true,
+                        borderless: true,
+                        underlineColorAndroid: 'transparent',
+                        password: true,
+                        viewPass: true,
+                      }}
+                      component={renderInput}
+                    />
+                  </Block>
+                  <Block width={width * 0.8} style={{ marginBottom: 15 }}>
+                    <Field
+                      name={'newPassword2'}
+                      props={{
+                        placeholder: 'Confirm new password...',
+                        rounded: true,
+                        borderless: true,
+                        underlineColorAndroid: 'transparent',
+                        password: true,
+                        viewPass: true,
+                      }}
+                      component={renderInput}
+                    />
+                  </Block>
+
+                  <Block middle>
+                    {
+                      isChangingPassword ? (
+                        <AnimatedLoader visible={true} overlayColor="rgba(255,255,255,0.75)" animationStyle={styles.lottie} speed={1} />
+                      ) : (
+                          <Button round color="primary" style={styles.createButton} onPress={handleSubmit(onChangePassword)}>
+                            <Text bold size={14} color={theme.COLORS.WHITE}>
+                              CHANGE PASSWORD
+                                  </Text>
+                          </Button>
+                        )
+                    }
+
+                    {
+                      error && (
+                        <Text>{error}</Text>
+                      )
+                    }
+
+                  </Block>
+                </KeyboardAvoidingView>
+              </Block>
+            </Block>
+          </Block>
+        </Block>
+      </ImageBackground>
+    </Block>
+
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20
+  registerContainer: {
+    width: width * 0.9,
+    height: height * 0.5,
+    backgroundColor: "#F4F5F7",
+    borderRadius: 4,
+    shadowColor: theme.COLORS.BLACK,
+    shadowOffset: {
+      width: 0,
+      height: 4
+    },
+    marginTop: -64,
+    shadowRadius: 8,
+    shadowOpacity: 0.1,
+    elevation: 1,
+    overflow: "hidden"
   },
-  title: {
-    fontSize: AppStyles.fontSize.title,
-    fontWeight: 'bold',
-    marginTop: 20,
-    marginBottom: 20,
+  socialConnect: {
+    backgroundColor: theme.COLORS.WHITE,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderColor: "#8898AA"
   },
-  centerTitle: {
-    alignSelf: 'stretch',
-    textAlign: 'center',
+  socialButtons: {
+    width: 120,
+    height: 40,
+    backgroundColor: "#fff",
+    shadowColor: theme.COLORS.BLACK,
+    shadowOffset: {
+      width: 0,
+      height: 4
+    },
+    shadowRadius: 8,
+    shadowOpacity: 0.1,
+    elevation: 1
   },
-  lottie: { 
-    width: 100, 
-    height: 100, 
+  socialTextButtons: {
+    color: theme.COLORS.PRIMARY,
+    fontWeight: "800",
+    fontSize: 14
   },
-  input: {
-    marginVertical: 4,
+  inputIcons: {
+    marginRight: 12
   },
-  button: {
-    marginVertical: 8,
+  passwordCheck: {
+    paddingLeft: 15,
+    paddingTop: 13,
+    paddingBottom: 30
   },
+  createButton: {
+    width: width * 0.5,
+    marginTop: 25
+  }
 });
 
 const mapStateToProps = state => ({
@@ -134,7 +259,7 @@ const mapDispatchToProps = dispatch => ({
       alert('Please fill out the required fields.');
       return;
     }
-    dispatch(actions.startChangingPassword({newPassword1, newPassword2, oldPassword}));
+    dispatch(actions.startChangingPassword({ newPassword1, newPassword2, oldPassword }));
   }
 })
 
@@ -142,5 +267,5 @@ export default connect(
   mapStateToProps,
   mapDispatchToProps
 )(
-  reduxForm({ form: 'change-password-form'})(ChangePassword)
+  reduxForm({ form: 'change-password-form' })(ChangePassword)
 );
